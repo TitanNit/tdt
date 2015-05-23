5b425b4ab28ab8e5a0a5d06ce29b60a223b99469
titanobi <_obiwan_@web.de>
2015-05-23 18:05:10 +0200

    fix

--- tdt/cvs/cdk/make/contrib-libs.mk
+++ tdt/cvs/cdk/make/contrib-libs.mk
@@ -807,7 +807,7 @@ $(DEPDIR)/ffmpeg: bootstrap libass rtmpdump @DEPENDS_ffmpeg@
 			--prefix=/usr && \
 		$(MAKE) && \
 		@INSTALL_ffmpeg@
-#	@DISTCLEANUP_ffmpeg@
+	@DISTCLEANUP_ffmpeg@
 	touch $@
 
 $(DEPDIR)/ffmpeg_old: bootstrap libass rtmpdump @DEPENDS_ffmpeg_old@
@@ -2291,7 +2291,7 @@ $(DEPDIR)/gmediarender: libupnp @DEPENDS_gmediarender@
 		LD="$(target)-gcc -L/home/aaf-svn/flashimg/source.titan/titan" \
 		$(MAKE) all && \
 		@INSTALL_gmediarender@
-#	@DISTCLEANUP_gmediarender@
+	@DISTCLEANUP_gmediarender@
 	touch $@
 
 #			--with-libupnp=$(targetprefix)/usr
